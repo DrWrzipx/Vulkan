65b6564b52fd940d6d954591f8eba9c20486ad4a
Dr_Matic <matic.drobez@gmail.com>
2018-11-06 20:41:50 +0100

    Add some debug possibilities in code.

--- Example/Renderer.cpp
+++ Example/Renderer.cpp
@@ -2,18 +2,28 @@
 #include <cstdlib>
 #include <assert.h>
 #include <vector>
+#include <iostream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif // _WIN32
+
 
 
 Renderer::Renderer()
 {
+	_SetupDebug();
 	_InitInstance();
+	_InitDebug();
 	_InitDevice();
+	
 }
 
 
 Renderer::~Renderer()
 {
 	_DeInitDevice();
+	_DeInitDebug();
 	_DeInitInstance();
 }
 
@@ -21,13 +31,19 @@ void Renderer::_InitInstance()
 {
 	VkApplicationInfo application_info {};
 	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	application_info.apiVersion = VK_API_VERSION_1_0;
+	application_info.apiVersion = VK_MAKE_VERSION(1, 0, 3);
 	application_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
 	application_info.pApplicationName = "Vulkan Tutorial 1";
 
 	VkInstanceCreateInfo instance_create_info {};
 	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	instance_create_info.pApplicationInfo = &application_info;
+	
+	instance_create_info.enabledLayerCount = _instance_layers.size();
+	instance_create_info.ppEnabledLayerNames = _instance_layers.data();
+	instance_create_info.enabledExtensionCount = _instance_extensions.size();
+	instance_create_info.ppEnabledExtensionNames = _instance_extensions.data();
+	instance_create_info.pNext = &debug_callback_create_info;
 
 	auto err = vkCreateInstance(&instance_create_info, nullptr, &_instance);
 
@@ -73,6 +89,33 @@ void Renderer::_InitDevice()
 		}
 	}
 
+	{
+		uint32_t layer_count = 0;
+		vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+		std::vector<VkLayerProperties> layer_property_list(layer_count);
+		vkEnumerateInstanceLayerProperties(&layer_count, layer_property_list.data());
+
+		std::cout << "Instance Layers: \n";
+		for (auto &i : layer_property_list) {
+			std::cout << " " << i.layerName << "\t\t | " << i.description << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+
+	{
+		uint32_t layer_count = 0;
+		vkEnumerateDeviceLayerProperties(_gpu, &layer_count, nullptr);
+		std::vector<VkLayerProperties> layer_property_list(layer_count);
+		vkEnumerateDeviceLayerProperties(_gpu, &layer_count, layer_property_list.data());
+
+		std::cout << "Device Layers: \n";
+		for (auto &i : layer_property_list) {
+			std::cout << " " << i.layerName << "\t\t | " << i.description << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
 	float queue_priorities[]{ 1.0f };
 	VkDeviceQueueCreateInfo device_queue_create_info {};
 	device_queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
@@ -97,3 +140,92 @@ void Renderer::_DeInitDevice()
 	vkDestroyDevice(_device, nullptr);
 	_device = nullptr;
 }
+
+VKAPI_ATTR VkBool32 VKAPI_CALL
+VulkanDebugCallback(
+	VkDebugReportFlagsEXT flags,
+	VkDebugReportObjectTypeEXT object_type,
+	uint64_t src_object,
+	size_t location,
+	int32_t msg_code,
+	const char* layer_prefix,
+	const char* msg,
+	void * user_data )
+{
+	//std::ostringstream stream;
+	std::cout << "VKDBG: ";
+	if (flags & VK_DEBUG_REPORT_INFORMATION_BIT_EXT) 
+	{
+		std::cout << "INFO: ";
+	}
+	if (flags & VK_DEBUG_REPORT_WARNING_BIT_EXT) {
+		std::cout << "WARNING: ";
+	}
+	if (flags & VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT) {
+		std::cout << "PERFORMANCE: ";
+	}
+	if (flags & VK_DEBUG_REPORT_ERROR_BIT_EXT) {
+		std::cout << "ERROR: ";
+	}
+	if (flags & VK_DEBUG_REPORT_DEBUG_BIT_EXT) {
+		std::cout << "DEBUG: ";
+	}
+	std::cout << "@[" << layer_prefix << "]:";
+	std::cout << msg << std::endl;
+
+#ifdef _WIN32
+	//MessageBox(NULL, stream.str().c_str(), "Vulkan Error!", 0);
+#endif
+
+	return false;
+}
+
+void Renderer::_SetupDebug()
+{
+	_instance_layers.push_back("VK_LAYER_LUNARG_standard_validation");
+	_instance_layers.push_back("VK_LAYER_LUNARG_screenshot");
+	_instance_layers.push_back("VK_LAYER_LUNARG_standard_validation");
+	_instance_layers.push_back("VK_LAYER_GOOGLE_threading");
+	_instance_layers.push_back("VK_LAYER_GOOGLE_unique_objects");
+	_instance_layers.push_back("VK_LAYER_LUNARG_vktrace");
+	//_instance_layers.push_back()
+
+	_instance_extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+}
+
+PFN_vkCreateDebugReportCallbackEXT fvkCreateDebugReportCallbackEXT = nullptr;
+PFN_vkDestroyDebugReportCallbackEXT fvkDestroyDebugReportCallbackEXT = nullptr;
+
+void Renderer::_InitDebug()
+{
+	fvkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(_instance, "vkCreateDebugReportCallbackEXT");
+	fvkDestroyDebugReportCallbackEXT = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(_instance, "vkDestroyDebugReportCallbackEXT");
+	
+	if (nullptr == fvkCreateDebugReportCallbackEXT || nullptr == fvkDestroyDebugReportCallbackEXT) 
+	{
+		assert(1 && "Vulkan ERROR: Can not fetch debug function pointers.");
+		std::exit(-1);
+	}
+
+	
+	debug_callback_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+	debug_callback_create_info.pfnCallback = &VulkanDebugCallback;
+	debug_callback_create_info.flags =
+		VK_DEBUG_REPORT_INFORMATION_BIT_EXT |
+		VK_DEBUG_REPORT_WARNING_BIT_EXT |
+		VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT |
+		VK_DEBUG_REPORT_ERROR_BIT_EXT |
+		VK_DEBUG_REPORT_DEBUG_BIT_EXT |
+		VK_DEBUG_REPORT_FLAG_BITS_MAX_ENUM_EXT |
+		0;
+	fvkCreateDebugReportCallbackEXT(_instance, &debug_callback_create_info, nullptr, &_debug_report);
+	
+	// vkCreateDebugReportCallbackEXT(_instance, nullptr, nullptr, nullptr);
+}
+
+void Renderer::_DeInitDebug()
+{
+	fvkDestroyDebugReportCallbackEXT(_instance, _debug_report, nullptr);
+	//_debug_report = nullptr;
+}
+
--- Example/Renderer.h
+++ Example/Renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vector>
 
 class Renderer
 {
@@ -15,11 +16,21 @@ private:
 	void _InitDevice();
 	void _DeInitDevice();
 
+	void _SetupDebug();
+	void _InitDebug();
+	void _DeInitDebug();
+
 	VkInstance _instance = nullptr;
 	VkDevice _device = nullptr;
 	VkPhysicalDevice _gpu = nullptr;
 	VkPhysicalDeviceProperties _gpu_properties {};
 	
 	uint32_t _graphics_familiy_index;
+
+	std::vector<const char*> _instance_layers;
+	std::vector<const char*> _instance_extensions;
+
+	VkDebugReportCallbackEXT _debug_report = nullptr;
+	VkDebugReportCallbackCreateInfoEXT debug_callback_create_info{};
 };
 
